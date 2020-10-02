a5f15a5119bca3146441978190d5e77e807a2740
Eden Johnson <50639814+edenjohnson@users.noreply.github.com>
2020-10-02 10:37:53 -0700

    Update Eden_Johnson_GA_TS.py
    adjusted number of children generated from crossover, and adjusted fitness metric calculation

--- Eden_Johnson_GA_TS.py
+++ Eden_Johnson_GA_TS.py
@@ -17,58 +17,82 @@ parent1 = [0, 1, 2, 3, 4, 5, 6, 7]
 parent2 = [7, 6, 5, 4, 3, 2, 1, 0]
 
 def crossover(parent1, parent2):
-    """ Performs crossover between two parents and returns a child. """
+    """ Performs crossover between two parents and returns 2 children. """
 
     window_size = random.randint(0, 7)
-    possible_starting_index = random.randint(0, (len(parent1) - window_size + 1))
+    possible_starting_index = random.randint(0, (8 - window_size + 1))
 
-    child = []
-    for i in range(len(parent1)):
-        child.append("-")
+    child1 = []
+    child2 = []
+    for i in range(8):
+        child1.append("-")
+        child2.append("-")
 
-    child[possible_starting_index:(possible_starting_index + window_size)] = parent1[possible_starting_index:(possible_starting_index + window_size)]
+    child1[possible_starting_index:(possible_starting_index + window_size)] = parent1[possible_starting_index:(possible_starting_index + window_size)]
+    child2[possible_starting_index:(possible_starting_index + window_size)] = parent2[possible_starting_index:(possible_starting_index + window_size)]
 
-    values_to_add = []
+    values_to_add_child_1 = []
+    values_to_add_child_2 = []
 
     for i in range(len(parent2)):
-        if parent2[i] not in child:
-            values_to_add.append(parent2[i])
+        if parent2[i] not in child1:
+            values_to_add_child_1.append(parent2[i])
+        if parent1[i] not in child2:
+            values_to_add_child_2.append(parent1[i])
 
-    for i in range(len(child)):
-        if child[i] == "-":
-            child[i] = values_to_add.pop(0)
+    for i in range(len(child1)):
+        if child1[i] == "-":
+            child1[i] = values_to_add_child_1.pop(0)
 
-    return child
+    for i in range(len(child2)):
+        if child2[i] == "-":
+            child2[i] = values_to_add_child_2.pop(0)
 
-child = crossover(parent1, parent2)
-print(child)
+    return [child1, child2]
+
+children = crossover(parent1, parent2)
 
 # perform mutation
-def mutation(child):
+def mutation(children):
     """ Performs point switch mutation on a child and returns the mutated child. """
-    possible_first_index = random.randint(0, 7)
-    possible_second_index = random.randint(0, 7)
-    first_value = child[possible_first_index]
-    second_value = child[possible_second_index]
-    child[possible_first_index] = second_value
-    child[possible_second_index] = first_value
-    return child
+    mutated_children = []
+    for child in children:
+        possible_first_index = random.randint(0, 7)
+        possible_second_index = random.randint(0, 7)
+        first_value = child[possible_first_index]
+        second_value = child[possible_second_index]
+        child[possible_first_index] = second_value
+        child[possible_second_index] = first_value
+        mutated_children.append(child)
+    return mutated_children
 
-mutated_child = mutation(child)
-print(mutated_child)
-
-new_city_list = []
-for i in range(len(mutated_child)):
-    city = index_dict[mutated_child[i]]
-    new_city_list.append(city)
+mutated_children = mutation(children)
 
 
-fitness_score = 0
+child1_fitness = 0
+child2_fitness = 0
+
+new_city_list_child_1 = []
+new_city_list_child_2 = []
+for i in range(len(mutated_children[0])):
+    city = index_dict[mutated_children[0][i]]
+    new_city_list_child_1.append(city)
+
 # look up distance metrics for child in table
-for i in range(len(new_city_list) - 1):
-    fitness_score += distance_df[new_city_list[i]][i + 1]
+for i in range(len(new_city_list_child_1) - 1):
+    child1_fitness += distance_df[new_city_list_child_1[i]][mutated_children[0][i + 1]]
 
-print(fitness_score)
+
+for i in range(len(mutated_children[1])):
+    city = index_dict[mutated_children[1][i]]
+    new_city_list_child_2.append(city)
+
+# look up distance metrics for child in table
+for i in range(len(new_city_list_child_2) - 1):
+    child2_fitness += distance_df[new_city_list_child_2[i]][mutated_children[1][i + 1]]
+
+
+
 
 def ga_call(generations, percent_population_selected, population_size):
     """ Function calls the genetic algorithm for the amount of times specified. """
